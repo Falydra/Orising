7b05880cc5e09b394a413e418a0744c0c952cad1
Aly <100826322+Falydra@users.noreply.github.com>
2024-02-15 09:35:42 +0700

    Update api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 export const getToken = async () => {
-  const CLIENT_ID = "613f22337e454a6a8787c09e67acdf10"; // Replace with your Spotify client ID
-  const REDIRECT_URI = "http://localhost:5173/callback"; // Ensure this matches the redirect URI in your Spotify settings
-  const SCOPE = "user-read-private user-read-email"; // Add any scopes you need here
+  const CLIENT_ID = "613f22337e454a6a8787c09e67acdf10";
+  const REDIRECT_URI = "http://localhost:5173/callback";
+  const SCOPE = "user-read-private user-read-email"; 
 
   const response = await fetch("https://accounts.spotify.com/authorize", {
     method: "GET",
